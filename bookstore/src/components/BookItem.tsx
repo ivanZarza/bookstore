f7fc01ee4a585411ad016b6a7b422c812af0b309
ivanZarza <ivanzarzaestevez@gmail.com>
2025-03-14 09:50:17 +0100

    wip

--- bookstore/src/components/BookItem.tsx
+++ bookstore/src/components/BookItem.tsx
@@ -3,11 +3,11 @@
 function BookItem() {
   return (
     <>
-      <section className="w-full flex flex-col gap-4 p-10 items-center border-2 bg-indigo-100">
+      <section className="w-full flex flex-col gap-4 items-center border-1 bg-indigo-100">
         <img src="https://m.media-amazon.com/images/I/914VEoNzIPL._SL1500_.jpg" alt="Portada del libro El Hobbit" className="size w-80" />
 
-          <div className="w-full flex items-start justify-start border-2">
-        <h1 className="text-3xl p-6"> El Hobbit</h1>
+          <div className="w-full flex items-start justify-start">
+        <h1 className="text-3xl px-3"> El Hobbit</h1>
         </div>
         <div >
           <h4>J.R.R. Tolkien</h4>
--- bookstore/src/pages/BooksPage.tsx
+++ bookstore/src/pages/BooksPage.tsx
@@ -3,8 +3,8 @@ import BookItem from '../components/BookItem'
 function BooksPage() {
   return (
     <div className='w-full flex flex-col items-center justify-center h-screen border-2 '>
-      <h1>Books</h1>
-      <ul className='flex'>
+      <h1 className='text-7xl'>Books</h1>
+      <ul className='flex gap-24 '>
         <li><BookItem /></li>
         <li><BookItem /></li>
         <li><BookItem /></li>
