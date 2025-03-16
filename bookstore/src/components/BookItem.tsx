44524f2e0377dd4511b8200e95381aee7b8ccef9
ivanZarza <ivanzarzaestevez@gmail.com>
2025-03-16 09:20:38 +0100

    reto2

--- bookstore/src/components/BookItem.tsx
+++ bookstore/src/components/BookItem.tsx
@@ -1,23 +1,40 @@
+import { Book } from '../config/type'
+import { TbTrashFilled } from "react-icons/tb";
+import { RiEditLine } from "react-icons/ri";
 
+type BookItemsProps={
+  book: Book 
+}
 
-function BookItem() {
+function BookItem(props: BookItemsProps) {
+
+  const { book } = props;
+
+  function handleEditBook() {
+    console.log('Libro editado');
+  }
+
+  function handleDeleteBook() {
+    console.log('Libro borrado');
+  }
+
   return (
     <>
       <section className="w-full flex flex-col gap-4 items-center border-1 bg-indigo-100">
-        <img src="https://m.media-amazon.com/images/I/914VEoNzIPL._SL1500_.jpg" alt="Portada del libro El Hobbit" className="size w-80" />
+        <img src={book.photo} alt="Portada del libro El Hobbit" className="size w-65" />
 
           <div className="w-full flex items-start justify-start">
-        <h1 className="text-3xl px-3"> El Hobbit</h1>
+        <h1 className="text-3xl px-3">{book.title}</h1>
         </div>
-        <div >
-          <h4>J.R.R. Tolkien</h4>
-          <p >Ciencia Ficción</p>
+        <div className="w-full flex justify-between items-center p-2">
+          <h4 className='text-[1.3rem]'>{book.author}</h4>
+          <p className='bg-blue-300 px-3 py-1 rounded-full'>{book.type}</p>
         </div>
-          <div >
-          <p>Price: 19.90€</p>
-            <div >
-            <button >Edit</button>
-            <button >Elim</button>
+          <div className="w-full flex justify-between items-center p-2 text-[1rem]">
+          <p>{book.price}€</p>
+            <div className="flex justify-between items-center gap-1" >
+            <button onClick={handleEditBook} className='bg-indigo-700 text-emerald-800' ><RiEditLine size={11}/></button>
+            <button onClick={handleDeleteBook} className='bg-indigo-100 text-red-500'><TbTrashFilled size={11}/></button>
             </div>
           </div>
       </section>
---
+++ bookstore/src/pages/BookList.tsx
@@ -0,0 +1,48 @@
+import BookItem from '../components/BookItem'
+import { Book } from '../config/type' 
+
+function BooksPage() {
+
+  const book: Book = {
+    id_book:  1,
+    id_user: 1,
+    title:  'El Hobbitoooo',
+    author: 'J.R.R. Tolkien',
+    photo: 'https://m.media-amazon.com/images/I/914VEoNzIPL._SL1500_.jpg',
+    price: 19.90,
+    type: 'Tapa blanda' 
+  } 
+  const book2: Book = {
+    id_book:  2,
+    id_user: 1,
+    title:  'El Hobbit',
+    author: 'J.R.R. Tolkien',
+    photo: 'https://m.media-amazon.com/images/I/914VEoNzIPL._SL1500_.jpg',
+    price: 19.90,
+    type: 'Tapa blanda' 
+  } 
+  const book3: Book = {
+    id_book:  3,
+    id_user: 1,
+    title:  'El Hobbitooon',
+    author: 'J.R.R. Tolkien',
+    photo: 'https://m.media-amazon.com/images/I/914VEoNzIPL._SL1500_.jpg',
+    price: 19.90,
+    type: 'Tapa blanda' 
+  } 
+
+  const listBooks = [book,book2,book3]
+  const painList = listBooks.map(libro => <li key={libro.id_book}><BookItem book={libro} /></li>)
+  return (
+    <div className='w-full flex flex-col items-center justify-self-start border-2 gap-20 p-9'>
+      <div >
+      <h1 className='text-6xl'>Books</h1>
+      </div>
+      <ul className='flex gap-24 '>
+      {painList}
+      </ul>
+    </div>
+  );
+}
+export default BooksPage
+
