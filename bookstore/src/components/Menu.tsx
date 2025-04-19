13244a4389a506dd6c0203fdf627549b7618f6fe
ivanZarza <ivanzarzaestevez@gmail.com>
2025-04-19 21:28:12 +0200

    wip

--- bookstore/src/components/Menu.tsx
+++ bookstore/src/components/Menu.tsx
@@ -26,8 +26,8 @@ function Menu(/* props:MenuProps */) {
 
   return (
     <>
-      <nav className="flex flex-col  justify-around items-center  text-xl gap-2  w-full  p-4 h-200 bg-gradient-to-b bg-teal-300/90 to-white/90 backdrop-blur-lg
-                      md:text-xl md:gap-5 md:justify-end md:flex-row md:m-2 md:w-full md:bg-none md:h-fit
+      <nav className="flex flex-col  justify-around items-center  text-xl gap-2  w-full h-100 p-4  bg-gradient-to-b bg-teal-300/90 to-white/90 backdrop-blur-lg
+                      md:text-xl md:gap-5 md:justify-end md:flex-row md:m-2 md:w-full md:bg-none md:h-fit 
                       lg:text-4xl lg:gap-5" /* {clases} */>
         <NavLink to="/" >Home</NavLink>
         {!ifUser && <NavLink to="/login" >Login</NavLink>}
