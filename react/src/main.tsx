f1b36b2eabbccf0de30f16c0b6310f757db46bc3
ivanZarza <ivanzarzaestevez@gmail.com>
2025-04-29 12:44:04 +0200

    changed BrowserRouter by HasRouter

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import UserProvider from './contexts/UserProvider';
 import BooksProvider from './contexts/BooksProvider';
 import FavoriteProvider from './contexts/FavoriteProvider';
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <FavoriteProvider>
     <BooksProvider>
       <UserProvider>
@@ -16,5 +16,5 @@ createRoot(document.getElementById('root')!).render(
       </UserProvider>
     </BooksProvider>
     </FavoriteProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
