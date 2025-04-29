0ec1939859eeed2e8337178f665668826c82689d
ivanZarza <ivanzarzaestevez@gmail.com>
2025-04-29 15:18:21 +0200

    Actualizacion de despliegue

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(BrowserAnimationsModule, ToastrModule.forRoot({
+  providers: [provideRouter(routes, withHashLocation()), provideClientHydration(), importProvidersFrom(BrowserAnimationsModule, ToastrModule.forRoot({
     positionClass: 'toast-top-right',
     preventDuplicates: true,
     closeButton: true,
