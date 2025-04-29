7c22548b67cf59fffd4e30505a918ae400999a6f
ivanZarza <ivanzarzaestevez@gmail.com>
2025-04-29 12:55:12 +0200

    fix logo

--- react/src/components/Logo.tsx
+++ react/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Logo() {
   return <>
     <Link to="/">
-      <img src="/logazo.webp" alt="logo"
+      <img src="./logazo.webp" alt="logo"
         className="size-24
     md:size-35 "
       />
