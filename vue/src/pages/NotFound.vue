e4a8d7afd6d951617a0a177277ec4e3d02957108
ivanZarza <ivanzarzaestevez@gmail.com>
2025-05-12 10:17:17 +0200

    wip

--- vue/src/pages/NotFound.vue
+++ vue/src/pages/NotFound.vue
@@ -1,7 +1,11 @@
 <template>
-  <v-container class="text-center">
+  <v-container class="d-flex flex-column align-center justify-center h-100 w-60 ga-5">
     <v-row>
-      <v-col>
+      <v-col class="d-flex flex-column align-center justify-center h-100 w-60 ga-5">
+        <CabeceraGenerica
+          descriptivo="La página que estás buscando no existe"
+          titulo="404 - Página no encontrada"
+        />
         <h1 class="text-h2">404 - Página no encontrada</h1>
         <p>La página que estás buscando no existe.</p>
         <v-btn color="primary" @click="$router.push('/')">Volver al inicio</v-btn>
