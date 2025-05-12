ed9dc368e65a8f647e396fdc6e8a8bcd297b3b74
ivanZarza <ivanzarzaestevez@gmail.com>
2025-05-12 12:49:18 +0200

    wip

--- vue/src/components/FormGenerico.vue
+++ vue/src/components/FormGenerico.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <v-form class="h-auto d-flex flex-column w-100 w-sm-50">
+  <v-form class="h-auto d-flex flex-column w-75 w-md-50">
     <slot />
     <slot name="select" />
     <slot name="boton" />
