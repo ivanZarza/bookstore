17fdc3ca8ab16dfff0c2e863872c91e7b81e1f70
ivanZarza <ivanzarzaestevez@gmail.com>
2025-05-13 21:13:05 +0200

    wip

--- react/src/components/Footer.tsx
+++ react/src/components/Footer.tsx
@@ -6,9 +6,9 @@ function Footer() {
       <footer className="w-full flex justify-center bg-teal-300  items-center py-6 gap-6
                       sm:text-2xl  
                           lg:text-3xl lg:gap-10">
-        <a href="">Facebook</a>
-        <a href="">Instagram</a>
-        <a href="">Twiter</a>
+        <a href="" className="teal-300">Facebook</a>
+        <a href="" className="teal-300">Instagram</a>
+        <a href="" className="teal-300">Twiter</a>
       </footer>
     </>
   );
