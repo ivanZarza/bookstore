26b6c6c4225a42e10ce3ab8a9746a8310dcf3e23
ivanZarza <ivanzarzaestevez@gmail.com>
2025-05-26 21:40:03 +0200

    wip

--- server/src/app.js
+++ server/src/app.js
@@ -73,7 +73,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(sessionMiddleware); 
+/* app.use(sessionMiddleware);  */
 app.use(cookieParser());
 
 app.use((req, res, next) => {
